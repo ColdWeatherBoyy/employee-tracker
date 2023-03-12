4de99cd81bc91a145bacb9b96bf580222b5aff11
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-03-12 19:17:55 -0400

    adds schema for all three tables with relational keys

--- db/schema.sql
+++ db/schema.sql
@@ -11,14 +11,17 @@ CREATE TABLE department (
 CREATE TABLE role (
   id INT NOT NULL PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
-  salary decimal NOT NULL,
-  department_id: INT NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
   id INT NOT NULL PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id: INT NOT NULL,
-  manager_id: INT
+  role_id INT NOT NULL,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
